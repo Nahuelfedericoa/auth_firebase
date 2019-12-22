2dad982ac20880d2a0807b862c467293f8e37e07
Nahuel Federico <alegrenahuel@gmail.com>
2019-12-22 06:54:45 -0300

    Updates

---
+++ precache-manifest.577d4adaa15760ee7c2f2867280a0c90.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b1f7ed64a995c821a21627d7c02dca4",
+    "url": "/auth_firebase/index.html"
+  },
+  {
+    "revision": "7fc5cc093cc94c31e5fc",
+    "url": "/auth_firebase/static/css/main.7fdb1a57.chunk.css"
+  },
+  {
+    "revision": "8e2666635cac33d731e9",
+    "url": "/auth_firebase/static/js/2.15276d8f.chunk.js"
+  },
+  {
+    "revision": "b03343d1dfdf0242e0f4ba3806a19844",
+    "url": "/auth_firebase/static/js/2.15276d8f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7fc5cc093cc94c31e5fc",
+    "url": "/auth_firebase/static/js/main.7355d2ab.chunk.js"
+  },
+  {
+    "revision": "2946cc333342bcd30696",
+    "url": "/auth_firebase/static/js/runtime-main.05b6d43e.js"
+  }
+]);
